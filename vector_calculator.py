981fd69357ea412a5294abddbdec76e655dee614
vinicius-mourao <imegzin@gmail.com>
2026-03-10 18:04:52 -0400

    Corrected code for plotting vector

--- vector_calculator.py
+++ vector_calculator.py
@@ -70,5 +70,5 @@ def plot_vector(P1, P2):
 # Execute the main functions
 main_manual()
 main_numpy()
-P1, P2 = input_two_points()  # Get the points again for plotting
+P1, P2 = input_two_points()
 plot_vector(P1, P2)
